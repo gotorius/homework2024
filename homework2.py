ad2f96c19900c415516647f7b07e131a5e689bb8
Ryosuke Goto <140908802+gotorius@users.noreply.github.com>
2025-07-04 15:57:03 +0900

    Create homework2.py

---
+++ homework2.py
@@ -0,0 +1,7 @@
+# !!WRITE ME!!に処理を記入する（このセルのhomework関数のみを提出することに注意）
+def homework(width, depth, height):
+  if(width * depth * height >= 1000):
+    my_result = True
+  else:
+    my_result = False
+  return my_result
